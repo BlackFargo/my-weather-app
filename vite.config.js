cef845ed072c8012be8d31125c946322bb6fae83
Yurii <uriymulyar@gmail.com>
2025-04-14 18:14:37 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
-	// base: '/Weather-website',
+	base: '/my-weather/',
+	build: {
+		outDir: 'dist',
+	},
 })
